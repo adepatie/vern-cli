11ecc1018fb807b941166d1c7c20a608e8068498
John Josef <john.josef@gmail.com>
2014-06-06 08:08:56 -0400

    updated command structure

--- templates/project/index.js
+++ templates/project/index.js
@@ -1,14 +1,16 @@
 /*
  * Project actions
  */
-var fs = require('fs');
+var fs = require('fs-extra');
 module.exports = {
   copy: function(path, callback) {
     if(!path === '.') {
       if(!fs.statSync(path).isDirectory()) {
-        fs.mkdirSync(path, '0644');
+        fs.mkdirpSync(path, '0644');
       }
     }
+
+
     callback(null, null);
   }
 };
