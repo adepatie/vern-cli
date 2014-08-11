a4ebaca58760a3d02a99e20355c364b97d73f8f0
John Josef <john.josef@gmail.com>
2014-08-11 09:58:08 -0400

    authentication details for new projects

--- templates/project/template/app/vern/vern_config.sample.js
+++ templates/project/template/app/vern/vern_config.sample.js
@@ -8,17 +8,39 @@ module.exports = {
   models_dir: __dirname + '/models',
   CORS_HEADERS: [],
   // CORS_ORIGINS: ['*'],
+  registration_on: false,
+  pre_registration_on: false,
+  registration_activates: true,
+  pre_register_email_activation: false,
+  strict_logins: false,
+  reserved_usernames: [
+    'default',
+    'admin',
+    'administrator',
+    'moderator',
+    'root',
+    'system'
+  ],
+  available_permissions: [''],
+  default_group_privacy: 'private',
+  enable_user_group_policy: true,
+  create_user_group_on_registration: true,
+  max_groups_per_user: 0,
   env: {
   
     // Development
     development: {
+      defaults: {
+        default_admin: '{{defaultAdmin}}',
+        default_password: '{{defaultPassword}}'
+      },
       web_host: 'http://localhost',
       port: 3458,
       // Database, CouchDB
       dbdriver: 'mongodb',
       databases: {
         mongodb: {
-          db: 'mongodb://localhost/{{apiName}}',
+          db: 'mongodb://localhost/{{dbName}}',
           user: '',
           password: ''
         }
@@ -29,13 +51,17 @@ module.exports = {
     
     // Production
     production: {
+      defaults: {
+        default_admin: '{{defaultAdmin}}',
+        default_password: '{{defaultPassword}}'
+      },
       web_host: 'https://www.mydomain.com',
       port: 3458,
       // Database, CouchDB
       dbdriver: 'mongodb',
       databases: {
         mongodb: {
-          db: 'mongodb://localhost/{{apiName}}',
+          db: 'mongodb://localhost/{{dbName}}',
           user: '',
           password: ''
         }
